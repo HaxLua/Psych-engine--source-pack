6bbfe57cee24df6156367afc0fe427616b3c4520
jacob <jacobhot222@gmail.com>
2022-03-06 10:21:34 -0500

    Code Fix
    error lol

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -104,7 +104,7 @@ class CreditsState extends MusicBeatState
 			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",					'https://twitter.com/ninja_muffin99',	'F73838'],
 			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",						'https://twitter.com/PhantomArcade3K',	'FFBB1B'],
 			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",						'https://twitter.com/evilsk8r',			'53E52C'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",						'https://twitter.com/kawaisprite',		'6475F3']
+			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",						'https://twitter.com/kawaisprite',		'6475F3'],
 			[''],
 			['Ideas'],
 			['eren', 'eren', 'Loading Screen Idea', 'https://github.com/ErenKaya31', 'FFFFFF']
